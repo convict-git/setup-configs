01d3385bcac1da10fef5da1e968b2178921d53e1
Priyanshu Shrivastav <official.mr.convict@gmail.com>
2025-05-18 18:45:35 +0530

    Remove lua and vim from treesitter temp for BLOB to string bug in dropbar

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "c",  "cpp", "rust", "typescript", "tsx", "javascript", "java", "lua", "vim", "vimdoc", "markdown", "markdown_inline" },
+  ensure_installed = { "c",  "cpp", "rust", "typescript", "tsx", "javascript", "java", "markdown", "markdown_inline" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
