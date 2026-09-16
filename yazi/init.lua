63befce12e07c53c823008ce1bb877da16f839b0
Priyanshu Shrivastav <official.mr.convict@gmail.com>
2026-09-16 22:07:22 +0530

    Revert "[yazi] Adds git status, smart-enter, full-border, toggle-pane, mount, chmod"
    This reverts commit 5434afa2b4acd524651a239e9402b58a5506cf33.

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,13 +1,1 @@
 require("yamb"):setup {}
-
-require("git"):setup {
-  order = 1500,
-}
-
-require("smart-enter"):setup {
-  open_multi = true,
-}
-
-require("full-border"):setup {
-  type = ui.Border.ROUNDED,
-}
